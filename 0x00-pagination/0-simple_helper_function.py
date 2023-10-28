7eb98367d53883fbcec55525f45193d2e41197fe
lawalgodwin <godwinl200@gmail.com>
2023-10-28 14:43:18 +0100

    implement a simple helper function to get bothe start and end index as requested by the client

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -12,7 +12,7 @@ from typing import Tuple
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """return a tuple of size two containing a
     start index and an end index corresponding to the range of indexes
-    to return in a list for those particular pagination parameters
+    to return in a list
     """
     start_index = page_size * (page - 1)
     end_index = page * page_size
