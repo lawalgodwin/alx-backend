952b803a6e68d354d8ba5902f54b934d0fe96004
lawalgodwin <godwinl200@gmail.com>
2023-10-28 16:43:00 +0100

    Implement Hypermedia (HATEOAS) Pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -63,7 +63,7 @@ class Server:
             "page_size": len(result),
             "page": page,
             "data": result,
-            "next_page": (page + 1) if (page + 1) >= total_pages else None,
+            "next_page": (page + 1) if (page + 1) <= total_pages else None,
             "prev_page": (page - 1) if page - 1 else None,
             "total_pages": total_pages
         }
