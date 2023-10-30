20903f545dc43c94717d5d0320a7c59c73c42cb2
lawalgodwin <godwinl200@gmail.com>
2023-10-30 17:12:53 +0100

    Implement a basic cache

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Create a class BasicCache that inherits from BaseCaching and is a caching
+system:
+
+You must use self.cache_data - dictionary from the parent class BaseCaching
+This caching system doesn’t have limit
+def put(self, key, item):
+Must assign to the dictionary self.cache_data the item value for the key key.
+If key or item is None, this method should not do anything.
+def get(self, key):
+Must return the value in self.cache_data linked to key.
+If key is None or if the key doesn’t exist in self.cache_data, return None
+"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Implement a basic cache"""
+    def __init__(self):
+        """class constructor"""
+        super().__init__()
+
+    def put(self, key, item):
+        """implement parent class abstract put method"""
+        if key or item:
+            self.cache_data.update({key: item})
+
+    def get(self, key: str):
+        """Implement parent class abstract get method"""
+        return self.cache_data.get(key, None)
