f362958ae02281bb2c899748fb087041c4d859c7
lawalgodwin <godwinl200@gmail.com>
2023-11-01 18:13:35 +0100

    Debug task 2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -16,13 +16,13 @@ app = Flask(__name__)
 
 app.config.from_object(Config)
 
-barbel = Babel(app)
+babel = Babel(app)
 
 
-@barbel.localeselector
+@babel.localeselector
 def get_locale():
     """determine the best match with our supported languages"""
-    return request.accept_languages(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/', strict_slashes=False)
