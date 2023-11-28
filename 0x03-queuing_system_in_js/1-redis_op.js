e596ed2b882078f6200c3b8babef35a64a27ab10
lawalgodwin <godwinl200@gmail.com>
2023-11-28 11:26:07 +0100

    Perform basic operations with Node Redis client

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -1,17 +1,17 @@
 import redisClient from './0-redis_client';
-import redis from 'redis'
+import redis from 'redis';
 
 const setNewSchool = (schoolName, value) => {
-    redisClient.set(schoolName, value, redis.print);
-    // redisClient.set(schoolName, value, (err, reply) => redis.print(err, reply));
-}
+  redisClient.set(schoolName, value, redis.print);
+  // redisClient.set(schoolName, value, (err, reply) => redis.print(err, reply));
+};
 
 const displaySchoolValue = (schoolName) => {
-    redisClient.get(schoolName, (err, result) => {
-        if (err) throw Error(err.message)
-        console.log(result)
-    });
-}
+  redisClient.get(schoolName, (err, result) => {
+    if (err) throw Error(err.message);
+    console.log(result);
+  });
+};
 
 displaySchoolValue('Holberton');
 setNewSchool('HolbertonSanFrancisco', '100');
