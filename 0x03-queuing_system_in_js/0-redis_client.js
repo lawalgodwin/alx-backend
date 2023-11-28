10da3b763e24fbe88c6a230b977d655cc0218802
lawalgodwin <godwinl200@gmail.com>
2023-11-28 10:32:15 +0100

    Connect to redis server using  Node Redis Client

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', err => console.log('Redis Client Error', err));
